07beef641bfdc4f50514838a5e5e03f2345963c2
Marek Dědič <developer@dedic.eu>
2024-10-17 13:47:06 +0200

    Fixed vimrc indentation

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -27,7 +27,7 @@ vim.api.nvim_set_keymap('i', '<CR>', 'coc#pum#visible() ? coc#pum#confirm() : "<
 vim.api.nvim_command('autocmd CursorHold * call CocActionAsync(\'highlight\')') -- Automatically highlight all occurences of symbol under cursor
 vim.cmd("hi Pmenu ctermfg=white guibg=darkmagenta") -- Fix floating menu colors
 vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', {}) -- go to definition
-vim.api.nvim_set_keymap('n', 'gt', '<Plug>(coc-type-definition)', {}) -- go to definition
+vim.api.nvim_set_keymap('n', 'gt', '<Plug>(coc-type-definition)', {}) -- go to type definition
 vim.api.nvim_set_keymap('n', 'gi', '<Plug>(coc-implementation)', {}) -- go to implementation
 vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)', {}) -- Go to references
 vim.api.nvim_set_keymap('n', 'ft', ':call CocActionAsync("format")<CR>', {}) -- Format whole file
@@ -101,8 +101,8 @@ vim.o.splitright = true
 
 -- Undo history is not erased when vim is closed
 if vim.fn.has('persistent_undo') == 1 then
-	vim.o.undofile = true
-	vim.o.undodir = vim.fn.stdpath('data') .. '/undo'
+  vim.o.undofile = true
+  vim.o.undodir = vim.fn.stdpath('data') .. '/undo'
 end
 
 require('nvim-treesitter.configs').setup({
