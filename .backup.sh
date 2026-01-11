9261adfc80bddc1aa0835158ccc339b404ac4e84
Marek Dědič <developer@dedic.eu>
2026-01-11 15:29:12 +0100

    Only cleaning up backup files if successful

--- .backup.sh
+++ .backup.sh
@@ -24,26 +24,30 @@ LOCAL_DIR=$HOME
 KEEP_BACKUPS_FOR="90D"
 FULL_BACKUP_EVERY="30D"
 
-echo "REMOVE OLDER FILES:"
-duplicity \
- remove-older-than $KEEP_BACKUPS_FOR --force \
- --sign-key $SGN_KEY --encrypt-key $ENC_KEY \
- b2://${B2_KEY_ID}:${B2_KEY}@${B2_BUCKET}/${B2_DIR}
-
-echo "PERFORM THE BACKUP:"
+echo ">> PERFORM THE BACKUP:"
 duplicity \
  backup \
  --sign-key $SGN_KEY --encrypt-key $ENC_KEY \
  --full-if-older-than $FULL_BACKUP_EVERY \
  ${LOCAL_DIR} b2://${B2_KEY_ID}:${B2_KEY}@${B2_BUCKET}/${B2_DIR}
 
-echo "CLEANUP FAILURES:"
-duplicity \
- cleanup --force \
- --sign-key $SGN_KEY --encrypt-key $ENC_KEY \
- b2://${B2_KEY_ID}:${B2_KEY}@${B2_BUCKET}/${B2_DIR}
+BACKUP_STATUS=$?
 
-echo "COLLECTION STATUS:"
+if [ $BACKUP_STATUS -eq 0 ]; then
+	echo ">> REMOVE OLDER FILES:"
+	duplicity \
+	 remove-older-than $KEEP_BACKUPS_FOR --force \
+	 --sign-key $SGN_KEY --encrypt-key $ENC_KEY \
+	 b2://${B2_KEY_ID}:${B2_KEY}@${B2_BUCKET}/${B2_DIR}
+
+	echo ">> CLEANUP FAILURES:"
+	duplicity \
+	 cleanup --force \
+	 --sign-key $SGN_KEY --encrypt-key $ENC_KEY \
+	 b2://${B2_KEY_ID}:${B2_KEY}@${B2_BUCKET}/${B2_DIR}
+fi
+
+echo ">> COLLECTION STATUS:"
 duplicity \
  collection-status \
  --sign-key $SGN_KEY --encrypt-key $ENC_KEY \
@@ -62,5 +66,5 @@ unset SIGN_PASSPHRASE
 unset KEEP_BACKUPS_FOR
 unset FULL_BACKUP_EVERY
 
-echo "SUCCESS!"
+echo ">> BACKUP SUCCESSFUL"
 echo $(date)
